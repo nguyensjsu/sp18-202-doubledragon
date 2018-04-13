bb4dedc8f59e5e6fa4f9d82978c9ef5bbe6cdb48
monanks <mcsavaliya.7401@gmail.com>
2018-04-13 15:49:02 -0700

    Added WelcomeInstruction World

---
+++ HungryPopeye/WelcomeInstructions.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class WelcomeInstructions here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WelcomeInstructions extends World
+{
+
+    /**
+     * Constructor for objects of class WelcomeInstructions.
+     * 
+     */
+    public WelcomeInstructions()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(900, 500, 1); 
+        showMessage();
+    }
+    
+    public void showMessage(){
+        GreenfootImage bg = getBackground();
+        
+        
+    }
+}
