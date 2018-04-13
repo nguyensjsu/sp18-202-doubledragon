3999911b5f4d0b88a9d9a34e78970555b17aa0f0
Ashish Virani <ashishvirani10@gmail.com>
2018-04-13 15:52:02 -0700

    Game State Boilerplate Code

---
+++ HungryPopeye/GameState.java
@@ -0,0 +1,31 @@
+import greenfoot.*;
+
+/*
+    Game State Class for handling various states that game will have by implementing IGameState Interface.
+*/
+
+
+public class GameState implements IGameState
+{
+
+    public GameState()
+    {    
+        
+    }
+    
+    public void start()
+    {
+        
+    }
+    
+    public void doJumping()
+    {
+        
+    }
+    
+    public void die()
+    {
+        
+    }
+    
+}
