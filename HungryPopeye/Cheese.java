18d2b91d3fcdab9118b6eaf7a739c815592a34d8
monanks <mcsavaliya.7401@gmail.com>
2018-04-15 12:48:53 -0700

    Added Obstacles: Olive

--- HungryPopeye/Cheese.java
+++ HungryPopeye/Cheese.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Paneer extends Obstacles
+public class Cheese extends Obstacles
 {
     /**
      * Act - do whatever the Paneer wants to do. This method is called whenever
