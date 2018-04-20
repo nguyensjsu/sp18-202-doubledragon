f8b1f007d7ec2e71f747527fec487c45c1493bfa
monanks <mcsavaliya.7401@gmail.com>
2018-04-20 10:02:01 -0700

    Added Business Logic into popeye class

--- HungryPopeye/Popeye.java
+++ HungryPopeye/Popeye.java
@@ -8,12 +8,88 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Popeye extends Actor
 {
-    /**
-     * Act - do whatever the Popeye wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    private int deltaX;         // x movement speed
+    private int deltaY;         // y movement speed
+    private int count = 2;
+    private Popeye popeye;
+    
+    private boolean stuck = true;   // stuck to paddle
+    //ConcreteSubject concretesubject = new ConcreteSubject();
+    private ConcreteSubject concretesubject;
+    GameManager gm = new GameManager();
+    
+    public void setDeltaX(int deltaX) {
+       this.deltaX = deltaX;
+    }
+
+    public int getDeltaX() {
+       return deltaX;
+    }
+    
+    public void setDeltaY(int deltaY) {
+       this.deltaY = deltaY;
+    }
+
+    public int getDeltaY() {
+       return deltaY;
+    }
+    
     public void act() 
     {
         // Add your action code here.
-    }    
+        if (!stuck)
+        {
+            move();
+            makeSmoke();
+            if(((Board) getWorld()).endScreen())
+            {
+                verticalBounce();
+            }
+            else
+            {
+                checkOut();
+            }
+        }
+    }
+    
+    public void move()
+    {
+        setLocation (getX() + deltaX, getY() + deltaY);
+        CollisionHandler objHandler;
+        checkCollision();
+        /*checkWalls();
+        checkPaddle();
+        checkBrick();
+        checkBonus();*/
+    }
+    
+    public void verticalBounce()
+    {
+        if (getY() == 0) 
+        {
+            deltaY = 7;
+            deltaX = Greenfoot.getRandomNumber(20) - 10;
+        }
+        if (getY() == getWorld().getHeight()-1)
+        {
+            deltaY = -7;
+            deltaX = Greenfoot.getRandomNumber(20) - 10;
+        }
+    }
+    
+    public void move(int dist)
+    {
+        setLocation (getX() + dist, getY());
+    }
+    
+    public void release()
+    {
+        deltaX = Greenfoot.getRandomNumber(11) - 5;
+        while(deltaX == 0)
+            deltaX = Greenfoot.getRandomNumber(11) - 5;
+            //deltaX = 2;
+        deltaY = -5;
+        stuck = false;
+        Greenfoot.playSound("Yahoo.wav");      
+    }
 }
