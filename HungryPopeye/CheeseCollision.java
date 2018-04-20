2efecae11d1639187852fd9c474fc21fad3df092
Jigar Soni <jigarsoni1111@gmail.com>
2018-04-20 16:10:06 -0700

    cheese-enter-check

--- HungryPopeye/CheeseCollision.java
+++ HungryPopeye/CheeseCollision.java
@@ -6,12 +6,27 @@ import greenfoot.*;
  * @author (your name) 
  * @version (a version number or a date)
  */
- public class CherryCollision extends Actor implements CollisionHandler
+ public class CheeseCollision extends Actor implements CollisionHandler
 {
     private CollisionHandler successor = null;
 
     public void setSuccessor(CollisionHandler next) {
         this.successor = next;
+    }
+
+    public void handleCollision(String objectName, Popeye objPopeye, int brickLocationX) {
+        if(objectName.equals("cheese")){
+            System.out.println("Entered into cheese check");
+            
+            //((Board)getWorld()).removeObject(brick);
+            // //((Board)getWorld()).incrementScore();
+            // Greenfoot.playSound("mp3");
+        }
+        else {
+            if(successor != null) {
+                successor.handleCollision(objectName, objBall, brickLocationX);
+            }    
+        } 	
     }
 }
 
