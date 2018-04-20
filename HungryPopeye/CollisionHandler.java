b4c3ed3cf138e642d0d6596270e748e52eea2f05
Jigar Soni <jigarsoni1111@gmail.com>
2018-04-19 21:27:31 -0700

    interface implementation collisionhandler

--- HungryPopeye/CollisionHandler.java
+++ HungryPopeye/CollisionHandler.java
@@ -1,4 +1,11 @@
-public interface CollisionHandler
+/**
+ * Write a description of class CollisionHandler here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface CollisionHandler  
 {
+    public void handleCollision(String objectName, Popeye objPopeye);
     
 }
