d96800ef8fed5c3783f4dc1767324430f88a5548
Jigar Soni <jigarsoni1111@gmail.com>
2018-04-20 23:58:40 -0700

    Trampoline

---
+++ HungryPopeye/Trampoline.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Trampoline here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Trampoline extends Actor
+{
+    /**
+     * Act - do whatever the Trampoline wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    private Popeye popeye;
+    public int numPop;
+    
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
